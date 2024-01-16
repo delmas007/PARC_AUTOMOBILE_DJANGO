3bef89b01853017965b06b5154c1e67ec9f5c978
delmas007 <azertyazerty1ze@gmail.com>
2024-01-16 11:44:50 +0000

    vehicule

---
+++ vehicule/forms.py
@@ -0,0 +1,81 @@
+from django import forms
+
+from Model.models import Vehicule
+
+
+class XYZ_DateInput(forms.DateInput):
+    input_type = "date"
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('attrs', {})
+        kwargs['attrs'].update({
+            'class': 'form-control',
+            'required': False,
+        })
+        kwargs["format"] = "%m-%d-%Y"
+        # kwargs["format"] = "%d-%m-%Y"
+        super().__init__(**kwargs)
+
+
+class VehiculeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['marque'].widget.attrs.update({
+            'type': "text",
+            'class': "form-control",
+            'id': "marque",
+            'placeholder': "Entrez la marque du vehicule",
+            'required': True,
+            'name': 'marque',
+        })
+        self.fields['modele'].widget.attrs.update({
+            'type': "text",
+            'class': "form-control",
+            'id': "modele",
+            'placeholder': "Entrez le modele du vehicule",
+            'required': True,
+            'name': 'modele',
+        })
+        self.fields['couleur'].widget.attrs.update({
+            'type': "text",
+            'class': "form-control",
+            'id': "couleur",
+            'placeholder': "Entrez la couleur du vehicule",
+            'required': True,
+            'name': 'couleur',
+        })
+        self.fields['numero_immatriculation'].widget.attrs.update({
+            'type': "text",
+            'class': "form-control",
+            'id': "marque",
+            'placeholder': "Entrez le numero d'immatriculation du vehicule",
+            'required': True,
+            'name': 'marque',
+        })
+        self.fields['type_carburant'].widget.attrs.update({
+            'class': "form-control",
+            'id': "exampleFormControlSelect1",
+            'placeholder': "Entrez la marque du vehicule",
+            'required': False,
+        })
+        self.fields['kilometrage'].widget.attrs.update({
+            'type': "number",
+            'class': "form-control",
+            'id': "marque",
+            'required': True,
+            'name': 'kilometrage',
+        })
+
+    class Meta:
+        model = Vehicule
+        fields = ['marque', 'modele', 'couleur', 'numero_immatriculation', 'date_mise_en_service', 'annee_fabrication',
+                  'type_carburant', 'kilometrage']
+
+        widgets = {
+            'date_mise_en_service': XYZ_DateInput(attrs={
+                'id': 'date',  # Utilisez l'ID généré par Django
+            }),
+            'annee_fabrication': XYZ_DateInput(attrs={
+                'id': 'date',  # Utilisez l'ID généré par Django
+            }),
+        }
--- vehicule/views.py
+++ vehicule/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
+from vehicule.forms import VehiculeForm
 
 
 # Create your views here.
 
 def Ajouter_vehicule(request):
-    return render(request, 'ajouter_vehicule.html')
+    context = {}
+    if request.method == 'POST':
+        # Associez l'utilisateur connecté à la partie employer du formulaire
+        form = VehiculeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Vehicule ajouté avec succès !')
+        else:
+            context['errors'] = form.errors
+
+    form = VehiculeForm()
+    context['form'] = form
+    return render(request, 'ajouter_vehicule.html', context=context)
