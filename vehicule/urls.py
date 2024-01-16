66c9210cffca87625b2b0d159146a6dbaa3d46a9
delmas007 <azertyazerty1ze@gmail.com>
2024-01-16 16:41:29 +0000

    vehicule

--- vehicule/urls.py
+++ vehicule/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from vehicule.views import Ajouter_vehicule, liste_vehicules, supprimer_vehicule, modifier_vehicule
+from vehicule.views import Ajouter_vehicule, liste_vehicules, supprimer_vehicule, modifier_vehicule, vehicules_pdf, \
+    vehicule_pdf
 
 app_name = 'vehicule'
 
 urlpatterns = [
     path('Ajouter_vehicule/', Ajouter_vehicule, name='Ajouter_vehicule'),
+    path('vehicule_pdf/<int:pk>/', vehicule_pdf, name='vehicule_pdf'),
+    path('vehicules_pdf/', vehicules_pdf, name='vehicules_pdf'),
     path('liste_vehicules/', liste_vehicules, name='liste_vehicules'),
     path('supprimer_vehicule/<int:pk>/', supprimer_vehicule, name='supprimer_vehicule'),
     path('modifier_vehicule/<int:pk>/', modifier_vehicule, name='modifier_vehicule'),
