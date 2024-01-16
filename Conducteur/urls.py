0687c9efe1866b6130009c2625d090e263c81373
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-01-16 16:42:29 +0000

    conducteur

--- Conducteur/urls.py
+++ Conducteur/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from Conducteur.views import ajouter_conducteur, tous_les_conducteurs, supprimer_conducteur, modifier_conducteur
+from Conducteur.views import ajouter_conducteur, tous_les_conducteurs, supprimer_conducteur, modifier_conducteur, \
+    conducteur_search, telecharger_pdf
 
 app_name = 'conducteur'
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('tous_les_conducteurs/', tous_les_conducteurs, name='tous_les_conducteurs'),
     path('supprimer_conducteur/<int:conducteur_id>/', supprimer_conducteur, name='supprimer_conducteur'),
     path('modifier_conducteur/<int:conducteur_id>/', modifier_conducteur, name='modifier_conducteur'),
+    path('recherche/', conducteur_search, name='conducteur_search'),
+    path('conducteur/<int:conducteur_id>/', telecharger_pdf, name='telecharger_pdf'),
 ]
