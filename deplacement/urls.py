372a233dcb4a341f54ff2d6e416b4605f1e3a76a
delmas007 <azertyazerty1ze@gmail.com>
2024-01-18 11:26:13 +0000

    deplacement

--- deplacement/urls.py
+++ deplacement/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from deplacement.views import enregistrer_deplacement, depart, liste_deplacement
+from deplacement.views import enregistrer_deplacement, depart, liste_deplacement, liste_deplacement_en_cours, arrivee, \
+    liste_deplacement_arrive
 
 app_name = 'deplacement'
 
@@ -8,5 +9,8 @@ urlpatterns = [
     path('enregistrer_deplacement/', enregistrer_deplacement, name='enregistrer_deplacement'),
     path('liste_deplacement/', liste_deplacement, name='liste_deplacement'),
     path('confirmer_depart/<int:pk>/', depart, name='depart'),
+    path('liste_deplacement_en_cours/', liste_deplacement_en_cours, name='liste_deplacement_en_cours'),
+    path('confirmer_arrive/<int:pk>/', arrivee, name='arrivee'),
+    path('liste_deplacement_arrive/', liste_deplacement_arrive, name='liste_deplacement_arrive'),
 
 ]
--- deplacement/views.py
+++ deplacement/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from datetime import datetime
 from Model.models import Deplacement
 from deplacement.forms import DeplacementForm
 
@@ -25,9 +25,25 @@ def liste_deplacement(request):
 def depart(request, pk):
     deplacement = get_object_or_404(Deplacement, pk=pk)
     deplacement.depart = True
+    deplacement.statut = 'en cours'
     deplacement.save()  # Ligne corrigée
     return redirect('deplacement:liste_deplacement')
 
+
 def liste_deplacement_en_cours(request):
     deplacement = Deplacement.objects.filter(depart=True, arrivee=False)
-    return render(request, 'afficher_deplacement.html', {'deplacements': deplacement})
+    return render(request, 'afficher_deplacement_en_cours.html', {'deplacements': deplacement})
+
+
+def arrivee(request, pk):
+    deplacement = get_object_or_404(Deplacement, pk=pk)
+    deplacement.arrivee = True
+    deplacement.statut = 'arrivée'
+    deplacement.save()  # Ligne corrigée
+    return redirect('deplacement:liste_deplacement_en_cours')
+
+
+def liste_deplacement_arrive(request):
+    today = datetime.now()
+    deplacement = Deplacement.objects.filter(depart=True, arrivee=True, date_arrivee__date=today)
+    return render(request, 'afficher_deplacement_arrive.html', {'deplacements': deplacement})
