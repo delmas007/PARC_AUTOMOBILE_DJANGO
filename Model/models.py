732f482e5c86335b6e0af48d120fac7f249ca58b
delmas007 <azertyazerty1ze@gmail.com>
2024-02-08 09:29:47 +0000

    model

--- Model/models.py
+++ Model/models.py
@@ -1,7 +1,80 @@
 # models.py
+import uuid
+
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.utils import timezone
 
 from django.db import models
+from enum import Enum
+from django_enum import EnumField
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError("Vous devez entrer un nom d'utilisateur")
+
+        user = self.model(
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(username=username, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class Role(Enum):
+    ADMIN = 'admin'
+    GESTIONNAIRE = 'gestionnaire'
+    CONDUCTEUR = 'conducteur'
+    CLIENT = 'client'
+
+
+class Profile(models.Model):
+    role = EnumField(Role)
+
+    def __str__(self):
+        return f"{self.role}"
+
+
+class Conducteur(models.Model):
+    nom = models.CharField(max_length=250)
+    prenom = models.CharField(max_length=250, )
+    numero_permis_conduire = models.CharField(max_length=20, unique=True, )
+    date_embauche = models.DateField(blank=True, null=True)
+    date_de_naissance = models.DateField(blank=True, null=True)
+    numero_telephone = models.CharField(max_length=15, )
+    adresse = models.TextField(blank=True, null=True)
+    disponibilite = models.BooleanField(default=True)
+    email = models.CharField(max_length=250, blank=True, null=True)
+    num_cni = models.CharField(max_length=250, )
+    image = models.ImageField(upload_to='ImagesConducteur/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nom} {self.prenom} - {self.numero_permis_conduire}"
+
+
+class Utilisateur(AbstractBaseUser):
+    mon_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    username = models.CharField(
+        unique=True,
+        max_length=255,
+        blank=False
+    )
+    nom = models.CharField(max_length=250, verbose_name='nom')
+    prenom = models.CharField(max_length=250)
+    conducteur = models.ForeignKey(Conducteur, on_delete=models.SET_NULL, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'username'
+    objects = MyUserManager()
 
 
 class Marque(models.Model):
@@ -12,6 +85,7 @@ class Marque(models.Model):
 
 
 class Vehicule(models.Model):
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True)
     marque = models.ForeignKey(Marque, on_delete=models.CASCADE)
     numero_immatriculation = models.CharField(max_length=250)
     couleur = models.CharField(max_length=250, blank=True, null=True)
@@ -39,22 +113,6 @@ class Vehicule(models.Model):
 
     def __str__(self):
         return f"{self.numero_immatriculation}"
-
-
-class Conducteur(models.Model):
-    nom = models.CharField(max_length=250)
-    prenom = models.CharField(max_length=250, )
-    numero_permis_conduire = models.CharField(max_length=20, unique=True, )
-    date_embauche = models.DateField(blank=True, null=True)
-    numero_telephone = models.CharField(max_length=15, )
-    adresse = models.TextField(blank=True, null=True)
-    disponibilite = models.BooleanField(default=True)
-    email = models.CharField(max_length=250, blank=True, null=True)
-    num_cni = models.CharField(max_length=250, )
-    image = models.ImageField(upload_to='ImagesConducteur/', null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.nom} {self.prenom} - {self.numero_permis_conduire}"
 
 
 class Deplacement(models.Model):
