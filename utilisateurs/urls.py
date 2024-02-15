91864be64b5d24ca1f9cc2d83ff0f58fcf20ee8d
delmas007 <azertyazerty1ze@gmail.com>
2024-02-15 11:01:59 +0000

    utilisateur

--- utilisateurs/urls.py
+++ utilisateurs/urls.py
@@ -1,6 +1,9 @@
-from utilisateurs.views import Accueil_user
+from utilisateurs.views import Accueil_user, inscription_user
 from django.urls import path
+
+app_name = 'utilisateur'
 
 urlpatterns = [
     path('Accueil', Accueil_user, name='Accueil_user'),
+    path('Inscription/', inscription_user, name='Inscription_user'),
 ]
--- utilisateurs/views.py
+++ utilisateurs/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+
+from Model.forms import UserRegistrationForm
+from Model.models import Roles
 
 
 # Create your views here.
 
 def Accueil_user(request):
     return render(request, 'index_user.html')
+
+
+@csrf_protect
+def inscription_user(request):
+    context = {}
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            client_role = Roles.objects.get(role=Roles.CLIENT)
+            user.roles = client_role
+            # user.is_active = False
+            # activateEmail(request, user, form.cleaned_data.get('email'))
+            user.save()
+            return redirect('Model:connexion')
+        else:
+
+            context['form'] = form
+            return render(request, 'inscription_client.html', context=context)
+
+    form = UserRegistrationForm()
+    context['form'] = form
+    return render(request, 'inscription_client.html', context=context)
