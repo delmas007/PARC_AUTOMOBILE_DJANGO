11bcdeb676ecbef233360c2297edf96909e2f1bc
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-02-19 13:05:22 +0000

    tous_les_conducteurs.html, Conducteur/form.py

--- Conducteur/forms.py
+++ Conducteur/forms.py
@@ -13,7 +13,7 @@ class ConducteurForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ConducteurForm, self).__init__(*args, **kwargs)
         for field_name in self.fields:
-            if field_name != 'disponibilite':
+            if field_name != 'disponibilite' and field_name != 'image':
                 self.fields[field_name].required = True
 
 
