e59cca8466484af47408abe12da838f861bff0b9
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-02-20 16:01:34 +0000

    conducteur/views.py

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from Admin.views import inscription, employer_compte, active_emp, desactive_amp
+from Admin.views import inscription, employer_compte, active_emp, desactive_amp, gestionnaire_inactifs
 
 app_name = 'admins'
 
 urlpatterns = [
     path('Ajout_gestionnaire/', inscription, name='Ajout_gestionnaire'),
     path('Compte_gestionnaire/', employer_compte, name='Compte_gestionnaire'),
+    path('gestionnaire_inactifs/', gestionnaire_inactifs, name='gestionnaire_inactifs'),
     path('Active_employer/<int:employer_id>/', active_emp, name='active_emp'),
     path('Desactive_employer/<int:employer_id>/', desactive_amp, name='desactive_amp'),
 
--- Admin/views.py
+++ Admin/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_protect
 
@@ -6,7 +5,6 @@ from Model.forms import UserRegistrationForm
 from Model.models import Roles, Utilisateur
 
 
-# Create your views here.
 @csrf_protect
 def inscription(request):
     context = {}
@@ -16,8 +14,6 @@ def inscription(request):
             user = form.save(commit=False)
             client_role = Roles.objects.get(role=Roles.GESTIONNAIRE)
             user.roles = client_role
-            # user.is_active = False
-            # activateEmail(request, user, form.cleaned_data.get('email'))
             user.save()
             return redirect('admins:Compte_gestionnaire')
         else:
@@ -32,19 +28,18 @@ def inscription(request):
 
 # @login_required
 def employer_compte(request):
-    # if not request.user.roles or request.user.roles.role != 'ADMIN':
-    #     return redirect('vitrine:Acces_interdit')
+    gestionnaires = Utilisateur.objects.filter(roles__role__in=[Roles.GESTIONNAIRE], is_active=True)
 
-    # Inclure à la fois les rôles "EMPLOYER" et "VENDEUR"
-    utilisateurs_gestionnaire = Utilisateur.objects.filter(roles__role__in=[Roles.GESTIONNAIRE])
+    return render(request, 'tous_les_gestionnaires.html', {'gestionnaires': gestionnaires})
 
-    return render(request, 'tous_les_gestionnaires.html', {'gestionnaires': utilisateurs_gestionnaire})
+
+def gestionnaire_inactifs(request):
+    gestionnaires2 = Utilisateur.objects.filter(roles__role__in=[Roles.GESTIONNAIRE], is_active=False)
+    return render(request, 'tous_les_gestionnaires.html', {'gestionnaires2': gestionnaires2})
 
 
 # @login_required
 def active_emp(request, employer_id):
-    # if not request.user.roles or request.user.roles.role != 'ADMIN':
-    #     return redirect('vitrine:Acces_interdit')
     employer = get_object_or_404(Utilisateur, id=employer_id)
     employer.is_active = True
     employer.save()
@@ -52,10 +47,7 @@ def active_emp(request, employer_id):
     return redirect('admins:Compte_gestionnaire')
 
 
-# @login_required
 def desactive_amp(request, employer_id):
-    # if not request.user.roles or request.user.roles.role != 'ADMIN':
-    #     return redirect('vitrine:Acces_interdit')
     employer = get_object_or_404(Utilisateur, id=employer_id)
     employer.is_active = False
     employer.save()
