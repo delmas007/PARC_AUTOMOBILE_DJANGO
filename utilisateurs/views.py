9a82b302f0d52dc8a18cc4548b47d38f1008f20a
delmas007 <azertyazerty1ze@gmail.com>
2024-02-22 11:01:53 +0000

    utilisateur

--- utilisateurs/views.py
+++ utilisateurs/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_protect
-
+from django.contrib import messages
 from Model.forms import UserRegistrationForm, ConnexionForm
 from Model.models import Roles, Utilisateur
 from utilisateurs.forms import ConducteurClientForm
@@ -16,20 +16,24 @@ def Accueil_user(request):
 
 def Compte(request):
     if request.method == 'POST':
-        utilisateur = Utilisateur.objects.filter(username=request.user.username)
-        conducteur_form = ConducteurClientForm(request.POST, request.FILES)
-        if conducteur_form.is_valid():
-            conducteur_instance = conducteur_form.save()
-            utilisateur.conducteur = conducteur_instance
-            messages.success(request, 'Le conducteur a été ajouté avec succès.')
-            return redirect('conducteur:ajouter_conducteur')
+        if request.user.is_authenticated:
+            utilisateur = Utilisateur.objects.get(username=request.user.username)
+            conducteur_form = ConducteurClientForm(request.POST, request.FILES)
+            if conducteur_form.is_valid():
+                conducteur_instance = conducteur_form.save()
+                utilisateur.conducteur = conducteur_instance
+                utilisateur.save()
+                messages.success(request, 'Le conducteur a été ajouté avec succès.')
+                return redirect('utilisateur:compte')
+            else:
+                print(conducteur_form.errors)
         else:
-            print(conducteur_form.errors)
+            return redirect('login')  # Rediriger vers la page de connexion si l'utilisateur n'est pas authentifié
     else:
         conducteur_form = ConducteurClientForm()
 
-    return render(request, 'compte.html',
-                  {'conducteur_form': conducteur_form})
+    return render(request, 'compte.html', {'conducteur_form': conducteur_form})
+
 @csrf_protect
 def inscription_user(request):
     context = {}
