719b622a90995769ed28a91f95240ed14e049c22
paulekra <ptrece8@gmail.com>
2024-02-26 13:02:55 +0000

    commit modifier deplacements

--- deplacement/forms.py
+++ deplacement/forms.py
@@ -19,20 +19,39 @@ class MultipleFileInput(Input):
 class DeplacementForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(DeplacementForm, self).__init__(*args, **kwargs)
+
+        # Récupérer l'instance de déplacement actuelle
+        instance = kwargs.get('instance')
+
+        # Définir le queryset pour le champ 'vehicule'
+        if instance:
+            # Si une instance existe (modification), inclure le véhicule/le conducteur actuel
+            vehicule_actuel = instance.vehicule
+            conducteur_actuel = instance.conducteur
+            self.fields['vehicule'].queryset = Vehicule.objects.filter(Q(disponibilite=True) | Q(pk=vehicule_actuel.pk))
+            self.fields['conducteur'].queryset = Conducteur.objects.filter(Q(disponibilite=True) | Q(pk=conducteur_actuel.pk))
+        else:
+            # Si aucune instance n'existe (ajout), afficher tous les véhicules/conducteurs disponibles
+            self.fields['vehicule'].queryset = Vehicule.objects.filter(disponibilite=True)
+            self.fields['conducteur'].queryset = Conducteur.objects.filter(disponibilite=True)
+
+        # Mise à jour des attributs du widget 'vehicule'
         self.fields['vehicule'].widget.attrs.update({
             'class': "form-control",
             'id': "selectVehicule",
             'required': True,
         })
-        self.fields['vehicule'].queryset = Vehicule.objects.exclude(disponibilite=False)
 
+        # Mise à jour des attributs du widget 'conducteur'
         self.fields['conducteur'].widget.attrs.update({
             'class': "form-control",
             'id': "selectVehicule2",
             'required': True,
         })
-        self.fields['conducteur'].queryset = Conducteur.objects.exclude(disponibilite=False)
-        
+
+        # Exclure les conducteurs non disponibles
+
+
     class Meta:
         model = Deplacement
         exclude = ['demande_prolongement_id']
