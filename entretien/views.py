4a16336a3694a2e8d6bba01e3917af3a27d37d92
paulekra <ptrece8@gmail.com>
2024-03-06 12:56:50 +0000

    commit entretien 3

--- entretien/views.py
+++ entretien/views.py
@@ -1,5 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.decorators.http import require_GET
+
 from entretien.forms import EntretienForm
 
 
@@ -19,3 +22,25 @@ def Ajouter_Entretien(request):
     else:
         form = EntretienForm()
     return render(request, 'enregistrer_entretient.html', {'form': form})
+
+
+class TypeEntretien:
+    pass
+
+
+@require_GET
+def get_type_entretien_data(request):
+    # Récupérer l'ID du type d'entretien fourni dans les paramètres de la requête
+    type_entretien_id = request.GET.get('type_entretien_id')
+
+    # Vérifier si l'identifiant du type d'entretien est fourni
+    if type_entretien_id is not None:
+        # Récupérer le type d'entretien correspondant à l'ID spécifié
+        try:
+            type_entretien = TypeEntretien.objects.get(pk=type_entretien_id)
+            data = {'nom': type_entretien.nom}
+            return JsonResponse(data)
+        except TypeEntretien.DoesNotExist:
+            return JsonResponse({'error': 'Type d\'entretien non trouvé'}, status=404)
+    else:
+        return JsonResponse({'error': 'Identifiant du type d\'entretien non spécifié'}, status=400)
