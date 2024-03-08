3ad7aee0872c4b497d7ae15611a79950397ec4bc
delmas007 <azertyazerty1ze@gmail.com>
2024-03-08 12:38:36 +0000

    entretien

--- parc_automobile/views.py
+++ parc_automobile/views.py
@@ -5,10 +5,10 @@ from Model.models import Incident
 
 
 def Accueil(request):
-    demande = Demande_prolongement.objects.all().filter(en_cours=True)
-    incidents = Incident.objects.filter(conducteur__isnull=False)
+    demande = Demande_prolongement.objects.filter(en_cours=True).order_by('-date_mise_a_jour')[:5]
+    incidents = Incident.objects.filter(conducteur__isnull=False).order_by('-date_mise_a_jour')[:5]
 
-    demande_compte = Demande_prolongement.objects.all().filter(en_cours=True).count()
-    incidents_compte = Incident.objects.filter(conducteur__isnull=False).count()
+    demande_compte = Demande_prolongement.objects.filter(en_cours=True).order_by('-date_mise_a_jour')[:5].count()
+    incidents_compte = Incident.objects.filter(conducteur__isnull=False).order_by('-date_mise_a_jour')[:5].count()
     total = demande_compte + incidents_compte
     return render(request, 'index.html', {'demandes': demande, 'incidents': incidents, 'total': total})
