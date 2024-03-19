148feb2346dadbaf12a9a91784d1dcd716ff4b05
yao ivan <yaoivan2002@gmail.com>
2024-03-19 14:54:50 +0000

    accueil

--- parc_automobile/views.py
+++ parc_automobile/views.py
@@ -27,27 +27,24 @@ def Accueil(request):
         Q(id__in=deplacements_etat_arrive_ids)).count()
 
     deplacement_termine = (
-        EtatArrive.objects.filter(date_arrive__gte=aujourd_hui - timedelta(days=7)).exclude(
-            date_arrive__gt=aujourd_hui).annotate(
-            hour=ExpressionWrapper(F('date_mise_a_jour'), output_field=fields.TimeField())
-        ).order_by('-hour')
-    )[:2]
+                              EtatArrive.objects.filter(date_arrive__gte=aujourd_hui - timedelta(days=7)).exclude(
+                                  date_arrive__gt=aujourd_hui).annotate(
+                                  hour=ExpressionWrapper(F('date_mise_a_jour'), output_field=fields.TimeField())
+                              ).order_by('-hour')
+                          )[:2]
 
     deplacement_en_cours = (
-        Deplacement.objects.filter(Q(date_depart__lte=aujourd_hui)).exclude(
-            Q(id__in=deplacements_etat_arrive_ids)).annotate(
-            hour=ExpressionWrapper(F('date_mise_a_jour'), output_field=fields.TimeField())
-        ).order_by('-hour')
-    )[:2]
+                               Deplacement.objects.filter(Q(date_depart__lte=aujourd_hui)).exclude(
+                                   Q(id__in=deplacements_etat_arrive_ids)).annotate(
+                                   hour=ExpressionWrapper(F('date_mise_a_jour'), output_field=fields.TimeField())
+                               ).order_by('-hour')
+                           )[:2]
 
     deplacement_en_attente = (
-        Deplacement.objects.filter(Q(date_depart__gt=aujourd_hui)).annotate(
-            hour=ExpressionWrapper(F('date_mise_a_jour'), output_field=fields.TimeField())
-        ).order_by('-hour')
-    )[:2]
-
-    # Récupérer les déplacements récents, par exemple, les 5 derniers
-    deplacements_recents = Deplacement.objects.order_by('-date_mise_a_jour')[:3]
+                                 Deplacement.objects.filter(Q(date_depart__gt=aujourd_hui)).annotate(
+                                     hour=ExpressionWrapper(F('date_mise_a_jour'), output_field=fields.TimeField())
+                                 ).order_by('-hour')
+                             )[:2]
 
     deplacements_planifies = Deplacement.objects.filter(date_depart__isnull=False)
 
@@ -75,28 +72,35 @@ def Accueil(request):
         'nombre_incidents': nombre_incidents,
         'nombre_deplacement_en_attente': nombre_deplacement_en_attente,
         'nombre_deplacement_termine': nombre_deplacement_termine,
-        'deplacements_recents': deplacements_recents,
         'vehicule_disponible': vehicules_page,
         'deplacements_planifies': deplacements_planifies,
         'now': aujourd_hui,
-        'api_key': os.getenv('API_KEY'),
         'deplacement_termine': deplacement_termine,
         'deplacement_en_cours': deplacement_en_cours,
         'deplacement_en_attente': deplacement_en_attente,
     }
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        vehicules_json = list(vehicules_page.object_list.values())
+        return JsonResponse({'vehicules': vehicules_json})
 
     return render(request, 'index.html', context)
 
 
 def deplacements_planifies(request):
     deplacements_planifies = Deplacement.objects.filter(date_depart__isnull=False)
+    conducteurs = Utilisateur.objects.exclude(conducteur_id__isnull=True).filter(is_active=True)
     out = []
     for event in deplacements_planifies:
         out.append({
-            'title': f"{event.vehicule.marque}-{event.conducteur.numero_permis_conduire}",
+            'title': f"{event.vehicule.marque}-{event.vehicule.type_commercial}-{event.vehicule.numero_immatriculation}",
             'id': event.id,
             'start': event.date_depart.strftime("%m/%d/%Y, %H:%M:%S"),
             'end': (event.date_depart + timedelta(hours=event.duree_deplacement)).strftime("%m/%d/%Y, %H:%M:%S"),
+            'duree': event.duree_deplacement,
+            # 'conducteur': conducteur
         })
 
     return JsonResponse(out, safe=False)
+
+
