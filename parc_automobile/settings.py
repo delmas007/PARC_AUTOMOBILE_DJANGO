71319210ab502250354f038ca9c4d7e54f6bedd3
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-04-09 09:22:39 +0000

    FINALs

--- parc_automobile/settings.py
+++ parc_automobile/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,15 +85,18 @@ WSGI_APPLICATION = 'parc_automobile.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'parc',
+#         'USER': 'postgres',
+#         'PASSWORD': '09102079Darius',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'parc',
-        'USER': 'postgres',
-        'PASSWORD': '09102079Darius',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 # Password validation
